8dc01a535a7c6c9c290f567446113d63edb01d8d
Jose Antonio Alvino Velasque <jalvinov@everis.com>
2021-01-06 11:24:16 -0500

    Swagger implemented

--- src/main/java/com/bank/people/PeopleApiApplication.java
+++ src/main/java/com/bank/people/PeopleApiApplication.java
@@ -1,13 +1,25 @@
 package com.bank.people;
 
+import com.bank.people.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PeopleApiApplication {
+@Import(SwaggerConfiguration.class)
+public class PeopleApiApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PeopleApiApplication.class, args);
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+	}
 }
--- src/main/java/com/bank/people/controller/PersonController.java
+++ src/main/java/com/bank/people/controller/PersonController.java
@@ -5,6 +5,10 @@ import com.bank.people.dto.PersonResponse;
 import com.bank.people.service.PersonService;
 import io.reactivex.Single;
 import io.reactivex.functions.Consumer;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/core/persons")
+@Api("/core/persons")
 public class PersonController
 {
     @Autowired
     private final PersonService personService;
 
     @GetMapping()
+    @ApiOperation(value = "Find person by Document Number", notes = "Retrieving a Person Class", response = PersonResponse.class)
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Success", response = PersonResponse.class)
+    })
     public Single<PersonResponse> getPersonByDocument(@RequestParam(name = "documentNumber") String document)
     {
         return personService.getPersonResponseByDocument(document);
